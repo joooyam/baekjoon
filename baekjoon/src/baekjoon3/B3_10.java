9d36c0f521e9c8acede561830af57ef28b2ce3ee
joooyam <ohgnar058@naver.com>
2025-03-11 00:55:19 +0900

    백준 ~B3_10

---
+++ baekjoon/src/baekjoon3/B3_10.java
@@ -0,0 +1,23 @@
+package baekjoon3;
+
+import java.util.*;
+
+public class B3_10 {
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+//      int n = sc.nextInt();
+
+      for (int i = 5; i >= 1; i--) {
+         for (int a = i - 1; a >= 1; a--) {
+            System.out.print("-");
+         }
+         for (int b = i; b <= 5; b++) {
+            System.out.print("*");
+         }
+         System.out.println();
+      }
+
+   }
+}
